22c673834513ab4b33d1107faebadc8016764ef6
purbayannath326-boop <purbayannath326@gmail.com>
2026-02-09 09:37:50 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+ const noBtn = document.getElementById("noBtn");
+const yesBtn = document.getElementById("yesBtn");
+
+// Make "No" button run away!
+noBtn.addEventListener("mouseover", () => {
+  const x = Math.random() * (window.innerWidth - noBtn.clientWidth);
+  const y = Math.random() * (window.innerHeight - noBtn.clientHeight);
+  noBtn.style.position = "absolute";
+  noBtn.style.left = `${x}px`;
+  noBtn.style.top = `${y}px`;
+});
+
+// When "Yes" is clicked
+yesBtn.addEventListener("click", () => {
+  document.body.innerHTML = `
+    <div class="container">
+      <h1>💖 Yay! You Said YES
+      ! 💖</h1>
+      <p>Happy Valentine’s Day ❤️</p>
+      <img src="https://media.giphy.com/media/3ohhwh6v" alt="Hearts ❤️">
+    </div>S0FJ9bXZmI/giphy.gif
+  `;
+});
